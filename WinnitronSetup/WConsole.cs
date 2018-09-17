0668a20ec272245f5bf324e0059a9ad49af8690f
Aaron Klaassen <aaron@outerspacehero.com>
2018-09-17 12:29:14 -0500

    WConsole class.

---
+++ WinnitronSetup/WConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WinnitronSetup {
+
+    public class WConsole {
+
+        public static void WriteLine(string text) {
+            Console.WriteLine(text);
+        }
+
+        public static void Warn(string msg) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\tWARNING: ");
+            Console.ResetColor();
+            Console.WriteLine(msg);
+        }
+
+        public static string Prompt(string txt = "") {
+            Console.Write(txt + "> ");
+            return Console.ReadLine().ToLower();
+        }
+    
+    }
+}
